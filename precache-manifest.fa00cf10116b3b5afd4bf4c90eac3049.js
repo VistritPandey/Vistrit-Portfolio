838184f505723a8bb44f750757ee986e9fad689a
vistritpandey <vistritpandey@gmail.com>
2020-12-09 09:28:01 +0530

    Updates

---
+++ precache-manifest.fa00cf10116b3b5afd4bf4c90eac3049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97d9927d6891bb83b4e00c76794e4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ebbb3330fed19ac0bc",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "33e6a020f9efaa4e46b3",
+    "url": "/static/js/2.c5444ead.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.c5444ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ebbb3330fed19ac0bc",
+    "url": "/static/js/main.42641c5d.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
